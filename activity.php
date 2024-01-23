4bebac0e501195ce7d2c0f9573946e5fea009f61
ItsPurple <83188626+PurpleWho@users.noreply.github.com>
2024-01-23 15:36:40 +0100

    Update activity.php
    Swedish Translation Done

--- activity.php
+++ activity.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'account-activity-log' => 'Account Activity Log',
-    'activity-log' => 'Activity Log',
-    'clear-filters' => 'Clear Filters',
+    'account-activity-log' => 'Konto Aktivitets Logg',
+    'activity-log' => 'Aktivitets Logg',
+    'clear-filters' => 'Rensa Filter',
     'metadata' => 'Metadata',
-    'close' => 'Close',
-    'no-logs-server' => 'No activity logs available for this server.'
+    'close' => 'Stäng',
+    'no-logs-server' => 'Inga aktivitetsloggar tillgängliga för den här servern.'
 ];
