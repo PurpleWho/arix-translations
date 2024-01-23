190c6e63d9bed91ee4fbc5abd6038ec15eb35b98
ItsPurple <83188626+PurpleWho@users.noreply.github.com>
2024-01-23 17:06:24 +0100

    Update users.php
    Swedish Translation Done

--- server/users.php
+++ server/users.php
@@ -1,32 +1,32 @@
 <?php
 
 return [
-    'title' => 'Users',
-    'manage-subusers' => 'Manage subusers',
-    'new-user' => 'New user',
+    'title' => 'Användare',
+    'manage-subusers' => 'Hantera Delanvändare',
+    'new-user' => 'Ny användare',
 
-    'no-users' => 'It looks like you don\'t have any subusers.',
+    'no-users' => 'Det verkar som att du inte har några delanvändare.',
 
-    'name' => 'Name',
-    'email' => 'Email',
-    '2FA-enabled' => '2FA Enabled',
-    'creation-date' => 'Creation date',
-    'modify-permissions' => 'Modify permissions',
+    'name' => 'Namn',
+    'email' => 'E-Post',
+    '2FA-enabled' => '2FA Aktiverad',
+    'creation-date' => 'Skapelsedagen',
+    'modify-permissions' => 'Ändra Behörigheter',
 
-    'user-email' => 'User Email',
-    'user-email-description' => 'Enter the email address of the user you wish to invite as a subuser for this server.',
+    'user-email' => 'Användarens E-Post',
+    'user-email-description' => 'Ange e-postadressen till den användare du vill bjuda in som delanvändare för denna server.',
 
-    'modify-permissions-for' => 'Modify permissions for',
-    'view-permissions-for' => 'View permissions for',
-    'create-new-subuser' => 'Create new subuser',
-    'must-not-exceed' => 'Email addresses must not exceed 191 characters.',
-    'valid-email' => 'A valid email address must be provided.',
-    'save' => 'Save',
-    'invite-user' => 'Invite User',
+    'modify-permissions-for' => 'Ändra behörigheter för',
+    'view-permissions-for' => 'Visa behörigheter för',
+    'create-new-subuser' => 'Skapa ny delanvändare',
+    'must-not-exceed' => 'E-Postadresser får inte överstiga 191 tecken.',
+    'valid-email' => 'En giltig e-postadress måste anges.',
+    'save' => 'Spara',
+    'invite-user' => 'Bjud In Användare',
 
-    'only-permissions-you-assigned' => 'Only permissions which your account is currently assigned may be selected when creating or modifying other users.',
+    'only-permissions-you-assigned' => 'Endast behörigheter som ditt konto för närvarande är tilldelat kan väljas när du skapar eller ändrar andra användare.',
 
-    'delete-this-subuser' => 'Delete this subuser?',
-    'yes-remove' => 'Yes, remove subuser',
-    'are-you-sure-to-remove' => 'Are you sure you wish to remove this subuser? They will have all access to this server revoked immediately.'
+    'delete-this-subuser' => 'Ta bort denna delanvändare?',
+    'yes-remove' => 'Ja, ta bort delanvändare',
+    'are-you-sure-to-remove' => 'Är du säker på att du vill ta bort denna delanvändare? De kommer att få all åtkomst till denna server borttaget omedelbart.'
 ];
